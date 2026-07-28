8ca415baf19efeb3794c0a83cc48ddf11be77bec
YasmimVitoria007 <vitoriiiayasmim@gmail.com>
2026-07-28 11:04:25 -0300

    Adicionanto categorias

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call(
             [
-                UsuarioSeeder::class
+                UsuarioSeeder::class,
+                CategoriaSeeder::class
             ]
         );
     }
